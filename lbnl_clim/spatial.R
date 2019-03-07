846492fa4b4979ea6cd0e94db84ab0dccbf97447
Rich Pauloo <richpauloo@gmail.com>
2019-03-07 14:27:50 -0800

    spatial preprocessing of lbnl climate change data for c2vsim

---
+++ lbnl_clim/spatial.R
@@ -0,0 +1,182 @@
+####################################################################################################
+# preprocess LBNL climate change data for TB and input to c2vsim model
+####################################################################################################
+
+# packages used
+library(sp)
+library(raster)
+library(dplyr)
+library(lubridate)
+library(tidyr)
+library(readr)
+library(maptools)
+library(ggplot2)
+library(fields)
+
+
+
+####################################################################################################
+# load data, subset to tulare basin, sanity checks to make sure data is correct
+####################################################################################################
+
+# load in all csv files
+fp <- list.files("C:/Users/rpauloo/Desktop/zhilin/tb/TB_CSV", full = TRUE)[-1]
+
+# read all data into a list, fill missing lat/lon, convert dates
+l <- vector("list", length = length(fp))
+for(i in 1:length(l)){
+  l[[i]] <- readr::read_csv(fp[[i]])
+}
+
+l <- do.call(rbind.data.frame, l)
+l <- tidyr::fill(l, LAT) %>% tidyr::fill(LONG)
+
+
+# convert to spatial data
+coords <- as.matrix(l[, c("LONG", "LAT")])
+spdf <- SpatialPointsDataFrame(coords = coords, 
+                               data   = l, 
+                               proj4string = CRS("+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
+
+# calcualte all ET
+spdf@data <- spdf@data %>% mutate(ALLET = QSOIL + QVEGE + QVEGT)
+
+# C2VSim elements shapefile: URL
+s <- shapefile("C:/Users/rpauloo/Desktop/zhilin/tb/c2vsim/C2Vsim_mesh.shp")
+
+# plot elements and points
+s <- spTransform(s, CRS("+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
+
+# plot(s)
+# points(spdf, col = "red", pch = 16)
+
+# subset to tulare basin: subbasins 14:21
+tb <- s[s@data$ISGE %in% 14:21, ]
+tb@data <- tb@data %>% dplyr::select(IE, ISGE) #only keep eleemnt id and sb id
+rownames(tb@data) <- NULL # remove rownames
+
+# plot(tb)
+# points(spdf, col = "red", pch = 16)
+
+
+# visualize first 12 months of precipt
+spdf@data %>% 
+  dplyr::filter(TIME %in% 197001:197012) %>% 
+  ggplot(aes(LONG, LAT, fill = PRECT)) +
+  geom_raster() + 
+  theme_minimal() +
+  scale_fill_viridis_c() +
+  facet_wrap(~TIME)
+  
+
+####################################################################################################
+# thin plate spline to interpolate points, return rasters for each var at each timestep
+####################################################################################################
+
+# interpolate each set of points 
+# first make a list of dataframes: each element is a unique time
+int <- split(spdf@data, f = spdf@data$TIME)
+
+interp <- function(df, var, extr){
+  tps <- Tps(df[, c("LONG", "LAT")], df[, var])
+  p   <- raster(tb, nrow = 100, ncol = 100) # res: 0.02008855, 0.02061255  (x, y)
+  p   <- interpolate(p, tps)
+  p   <- mask(p, tb)
+  if(extr == FALSE){
+    return(p)
+  } 
+  if(extr == TRUE){
+    rb  <- raster::extract(p, tb, sp = TRUE, fun = mean) # extract to tb elements
+    rb  <- rb@data
+    return(rb)
+  }   
+}
+
+# variables to get
+vars <- colnames(spdf@data)[4:9]
+
+# allocate
+prect <- vector("list", length = length(unique(spdf@data$TIME)))
+qover <- qsoil <- qvege <- qvegt <- allet <- prect
+ 
+st <- Sys.time()
+# calculate rasters
+for(i in 1:length(prect)){
+  prect[[i]] <- interp(int[[i]], vars[1], FALSE)
+  qover[[i]] <- interp(int[[i]], vars[2], FALSE)
+  qsoil[[i]] <- interp(int[[i]], vars[3], FALSE)
+  qvege[[i]] <- interp(int[[i]], vars[4], FALSE)
+  qvegt[[i]] <- interp(int[[i]], vars[5], FALSE)
+  allet[[i]] <- interp(int[[i]], vars[6], FALSE)
+}
+Sys.time() - st # took about 45 minutes
+
+# save
+readr::write_rds(prect, "C:/Users/rpauloo/Desktop/zhilin/tb/results/prect.rds") 
+readr::write_rds(qover, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qover.rds") 
+readr::write_rds(qsoil, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qsoil.rds") 
+readr::write_rds(qvege, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qvege.rds") 
+readr::write_rds(qvegt, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qvegt.rds") 
+readr::write_rds(allet, "C:/Users/rpauloo/Desktop/zhilin/tb/results/allet.rds") 
+
+
+
+####################################################################################################
+# extract rasters to c2vsim model ploygons and save as csv
+####################################################################################################
+
+# allocate
+prect_c2vsim <- vector("list", length = length(unique(spdf@data$TIME)))
+qover_c2vsim <- qsoil_c2vsim <- qvege_c2vsim <- qvegt_c2vsim <- allet_c2vsim <- prect_c2vsim
+
+st <- Sys.time()
+# calculate rasters
+for(i in 1:length(prect)){
+  prect_c2vsim[[i]] <- interp(int[[i]], vars[1], TRUE)
+  qover_c2vsim[[i]] <- interp(int[[i]], vars[2], TRUE)
+  qsoil_c2vsim[[i]] <- interp(int[[i]], vars[3], TRUE)
+  qvege_c2vsim[[i]] <- interp(int[[i]], vars[4], TRUE)
+  qvegt_c2vsim[[i]] <- interp(int[[i]], vars[5], TRUE)
+  allet_c2vsim[[i]] <- interp(int[[i]], vars[6], TRUE)
+}
+Sys.time() - st # took about 9 hours
+
+# add time
+for(i in 1:length(prect)){
+  prect_c2vsim[[i]]$time <- names(int)[i]
+  qover_c2vsim[[i]]$time <- names(int)[i]
+  qsoil_c2vsim[[i]]$time <- names(int)[i]
+  qvege_c2vsim[[i]]$time <- names(int)[i]
+  qvegt_c2vsim[[i]]$time <- names(int)[i]
+  allet_c2vsim[[i]]$time <- names(int)[i]
+}
+
+# save
+readr::write_rds(prect_c2vsim, "C:/Users/rpauloo/Desktop/zhilin/tb/results/prect_c2vsim.rds") 
+readr::write_rds(qover_c2vsim, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qover_c2vsim.rds") 
+readr::write_rds(qsoil_c2vsim, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qsoil_c2vsim.rds") 
+readr::write_rds(qvege_c2vsim, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qvege_c2vsim.rds") 
+readr::write_rds(qvegt_c2vsim, "C:/Users/rpauloo/Desktop/zhilin/tb/results/qvegt_c2vsim.rds") 
+readr::write_rds(allet_c2vsim, "C:/Users/rpauloo/Desktop/zhilin/tb/results/allet_c2vsim.rds") 
+
+
+####################################################################################################
+# compile into a single file 
+####################################################################################################
+
+v1 <- bind_rows(prect_c2vsim) %>% rename(PRECT = layer, TIME = time) %>% select(TIME, everything()) 
+v2 <- bind_rows(qover_c2vsim) %>% select(layer) %>% rename(QOVER = layer) 
+v3 <- bind_rows(qsoil_c2vsim) %>% select(layer) %>% rename(QSOIL = layer) 
+v4 <- bind_rows(qvege_c2vsim) %>% select(layer) %>% rename(QVEGE = layer) 
+v5 <- bind_rows(qvegt_c2vsim) %>% select(layer) %>% rename(QVEGT = layer) 
+v6 <- bind_rows(allet_c2vsim) %>% select(layer) %>% rename(ALLET = layer) 
+
+vf <- bind_cols(v1, v2, v3, v4, v5, v6)
+rm(v1, v2, v3, v4, v5, v6)
+
+# save
+readr::write_csv(vf, "C:/Users/rpauloo/Desktop/zhilin/tb/results/lbnl_clim_c2vsim.csv")
+
+  
+
+
