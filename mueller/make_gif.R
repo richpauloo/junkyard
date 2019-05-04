eba58f25319d6c44a75e496d2546dd8df1f3bd7c
Rich Pauloo <richpauloo@gmail.com>
2019-05-03 18:00:12 -0700

    Update make_gif.R

--- mueller/make_gif.R
+++ mueller/make_gif.R
@@ -2,17 +2,15 @@ library(magick)
 
 st <- Sys.time()
 
-report <- image_read_pdf("C:/Users/rpauloo/Desktop/mueller/mueller-report-searchable.pdf")
+#report <- image_read_pdf("https://cran.r-project.org/web/packages/pdftools/pdftools.pdf")
+report <- image_read_pdf("C:/Users/rpauloo/Documents/Github/junkyard/mueller/mueller-report-searchable.pdf")
 
 # add page numbers
 for(i in 1:length(report)){
-  report[i] <- image_annotate(report[i], paste0("page ", i, "/", length(report)), size = 200, gravity = "southwest", color = "pink")
+  report[i] <- image_annotate(report[i], paste0("page ", i, "/", length(report)), size = 200, gravity = "northwest", color = "pink")
 }
 
-
-anim <- image_animate(report)
-image_write(anim, "C:/Users/rpauloo/Desktop/mueller/mueller_pg.gif")
-
+image_write(report, "C:/Users/rpauloo/Documents/Github/junkyard/mueller/mueller_pg.gif", format = "gif")
 
 Sys.time() - st
 
